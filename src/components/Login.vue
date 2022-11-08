5d08236ab01202d6b23e3170191b0722e975e800
Aliaksandr Karpovich <alexsure.k@gmail.com>
2022-11-08 22:59:25 +0300

    Auth components incomplete

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    
+    <div class="auth-container">
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+.login-container {
+    width: 300px;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/login', component: Login },
+        { path: '/signup', component: Signup },
+    ],
+})
