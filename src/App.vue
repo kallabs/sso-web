9112f659d59e816c3321c920fa7d2f0bd44cbaec
Aliaksandr Karpovich <alexsure.k@gmail.com>
2022-11-13 23:27:49 +0300

    Api axios config & auth forms

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import api from '@/utils/api'
 
+onMounted(async () => {
+    console.log('App mounted')
+})
 </script>
 
 <template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,32 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router';
+import api from '@/utils/api'
 
+// reactive state
+const form = ref({
+    email: '',
+    password: '',
+})
+const $route = useRoute()
+
+async function onSubmit() {
+    const { data } = await api.post('/login', form.value)
+    console.log(data)
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${form.value}.`)
+  console.log($route.query)
+})
 </script>
 
 <template>
     
     <div class="auth-container">
         <div class="form-container login-container">
-            <form action="#">
+            <form @submit.prevent="onSubmit" action="#">
                 <h1>Log in</h1>
                 <!--
                 <div class="social-container">
@@ -17,10 +37,19 @@
                 
                 <span>or use your account</span>
                 -->
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <input 
+                    type="email" 
+                    placeholder="Email"
+                    v-model="form.email" />
+        
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    v-model="form.password" />
+
                 <a href="#">Forgot your password?</a>
-                <button>Log In</button>
+                
+                <button type="submit">Log In</button>
             </form>
         </div>
     </div>
